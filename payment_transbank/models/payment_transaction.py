996902e18ffeebb394dd6e40e1b3c54fb052ce9d
haular <aular.hector.dev@gmail.com>
2026-02-18 09:56:25 -0300

    refactor(payment_transbank): improve code formatting and streamline logic
    - Removed unused methods `_process_notification_data` and `_apply_updates`
      to declutter the codebase.
    - Updated `_extract_reference` method to handle Transbank-specific
      reference extraction logic.
    - Improved XML attribute formatting by removing unnecessary spaces
      for consistency.
    - Adjusted HTML attribute order in static description templates
      for better readability.

--- payment_transbank/models/payment_transaction.py
+++ payment_transbank/models/payment_transaction.py
@@ -8,7 +8,6 @@ _logger = logging.getLogger(__name__)
 class PaymentTransaction(models.Model):
     _inherit = 'payment.transaction'
 
-    # Common Transbank fields
     transbank_payment_type_code = fields.Char(string='Transbank Payment Type', readonly=True)
     transbank_response_code = fields.Integer(string='Transbank Response Code', readonly=True)
     transbank_status = fields.Char(string='Transbank Status', readonly=True)
@@ -16,25 +15,10 @@ class PaymentTransaction(models.Model):
     transbank_card_number = fields.Char(string='Card Last Digits', readonly=True)
 
     @api.model
-    def _get_tx_from_notification_data(self, provider_code, notification_data):
-        """Find the transaction based on the notification data."""
-        tx = super()._get_tx_from_notification_data(provider_code, notification_data)
-        if provider_code != 'transbank' or tx:
-            return tx
-
-        return self._search_by_reference(provider_code, notification_data)
-
-    def _process_notification_data(self, notification_data):
-        """Process the notification data and update the transaction."""
-        super()._process_notification_data(notification_data)
-        if self.provider_code != 'transbank':
-            return
-
-        self._apply_updates(notification_data)
-
-    def _apply_updates(self, payment_data):
-        """Placeholder for sub-modules to implement their specific logic."""
-        return
+    def _extract_reference(self, provider_code, payment_data):
+        if provider_code != 'transbank':
+            return super()._extract_reference(provider_code, payment_data)
+        return payment_data.get('reference')
 
     def _extract_amount_data(self, payment_data):
         if self.provider_code != 'transbank':
